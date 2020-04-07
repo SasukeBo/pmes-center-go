18cc1323fb5ee80674cbed84a216c4c3f107d342
Bob Wang <bob.wang@bindo.com>
2020-04-07 17:57:56 +0800

    need fetch

--- logic/lib.go
+++ logic/lib.go
@@ -6,7 +6,7 @@ import (
 	"github.com/SasukeBo/ftpviewer/orm"
 	"log"
 	"regexp"
-	"strconv"
+	"strings"
 	"time"
 
 	"github.com/SasukeBo/ftpviewer/ftpclient"
@@ -31,108 +31,41 @@ func IsMaterialExist(materialID string) bool {
 }
 
 // FetchMaterialDatas 根据料号从FTP服务器获取时间范围内数据
-func FetchMaterialDatas(material orm.Material, begin, end *time.Time) ([]int, error) {
+func fetchMaterialDatas(material orm.Material, files []FetchFile) ([]int, error) {
 	var fileIDs []int
-	cacheDays := 30
-	config := orm.GetSystemConfig("cache_days")
-	if config != nil {
-		if v, err := strconv.Atoi(config.Value); err == nil {
-			cacheDays = v
-		}
+
+	if len(files) == 0 {
+		return nil, errors.New("没有需要获取的数据文件")
 	}
 
-	today := time.Now()
-	cacheBegin := today.Add(time.Duration(int(time.Hour) * -24 * cacheDays))
-
-	if begin != nil && end != nil {
-		if begin.After(*end) {
-			return fileIDs, errors.New("获取数据的开始时间不能在结束时间之后")
-		}
-	} else if begin == nil && end == nil {
-		begin = &cacheBegin
-		end = &today
-	} else if begin != nil && end == nil {
-		if begin.After(today) {
-			return fileIDs, errors.New("获取数据时间范围不正确")
-		}
-		end = &today
-	} else if begin == nil && end != nil {
-		if end.Before(cacheBegin) {
-			return fileIDs, errors.New("获取数据时间范围不正确")
-		}
-		begin = &cacheBegin
+	xr := ftpclient.NewXLSXReader()
+	err := xr.ReadSize("./" + material.Name + "/" + files[0].File)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("读取数据文件%s失败", files[0].File))
 	}
+	handleSize(xr.DimSL, material.ID)
 
-	fileList, err := ftpclient.GetList("./" + material.Name)
-	if fe, ok := err.(*ftpclient.FTPError); ok {
-		fe.Logger()
-		return fileIDs, errors.New(fe.Message)
-	}
+	for _, file := range files {
+		xr := ftpclient.NewXLSXReader()
+		path := fmt.Sprintf("./%s/%s", material.Name, file.File)
 
-	reg := regexp.MustCompile(fileNamePattern)
-
-	// preHandleSize := false
-	for _, filename := range fileList {
-		matched := reg.FindAllStringSubmatch(filename, -1)
-		if len(matched) > 0 && len(matched[0]) > 4 {
-			xr := ftpclient.NewXLSXReader()
-			err := xr.ReadSize("./" + material.Name + "/" + filename)
-			if err != nil { // 如果读取失败
-				continue
+		fileList := orm.GetFileListWithPath(path)
+		if fileList == nil {
+			fileList = &orm.FileList{Path: path, MaterialID: material.ID, FileDate: file.Date}
+			orm.DB.Create(fileList)
+		}
+		fileIDs = append(fileIDs, fileList.ID)
+		go func() {
+			err := xr.Read(path)
+			if err != nil {
+				return
 			}
-			// preHandleSize = true
-			handleSize(xr.DimSL, material.ID)
-			break
-		}
-	}
-
-	for _, filename := range fileList {
-		matched := reg.FindAllStringSubmatch(filename, -1)
-		if len(matched) > 0 && len(matched[0]) > 4 {
-			path := "./" + material.Name + "/" + filename
-			dateStr := matched[0][3]
-			fileDate, _ := time.Parse(time.RFC3339, fmt.Sprintf("%s-%s-%sT00:00:00+08:00", dateStr[:4], dateStr[4:6], dateStr[6:]))
-			if fileIsNeed(path, &fileDate, begin, end) {
-				createDeviceIfNotExist(matched[0][2], material)
-				xr := ftpclient.NewXLSXReader()
-				fileList := orm.FileList{Path: path, MaterialID: material.ID, FileDate: fileDate}
-				orm.DB.Create(&fileList)
-				fileIDs = append(fileIDs, fileList.ID)
-				go func() {
-					err := xr.Read(path)
-					if err != nil {
-						return
-					}
-					xr.PathID = fileList.ID
-					ftpclient.PushStore(xr)
-				}()
-			}
-		}
+			xr.PathID = fileList.ID
+			ftpclient.PushStore(xr)
+		}()
 	}
 
 	return fileIDs, nil
-}
-
-func createDeviceIfNotExist(id string, material orm.Material) {
-	deviceName := fmt.Sprintf("%s设备%s", material.Name, id)
-	device := orm.GetDeviceWithName(deviceName)
-	if device == nil {
-		device = &orm.Device{
-			Name:       deviceName,
-			MaterialID: material.ID,
-		}
-		orm.DB.Create(device)
-	}
-}
-
-func fileIsNeed(path string, fileDate, begin, end *time.Time) bool {
-	if fl := orm.GetFileListWithPath(path); fl != nil {
-		return false
-	}
-	if begin == nil || end == nil {
-		return true
-	}
-	return begin.Before(*fileDate) && end.After(*fileDate)
 }
 
 func handleSize(dimSet map[string]ftpclient.SL, materialID int) {
@@ -155,10 +88,21 @@ func handleSize(dimSet map[string]ftpclient.SL, materialID int) {
 	tx.Commit()
 }
 
-func NeedFetch(m *orm.Material, begin, end *time.Time) bool {
+// FetchFile 需要获取的文件
+type FetchFile struct {
+	File string
+	Date time.Time
+}
+
+// NeedFetch 判断是否需要从FTP拉取数据
+// 给定料号，时间范围，对比数据库中已拉取文件路径，得出是否有需要拉取的文件路径
+func NeedFetch(m *orm.Material, begin, end *time.Time) ([]int, error) {
 	var conds []string
 	var vars []interface{}
-	conds = append(conds, "LIKE " + "%" + fmt.Sprint(m.ID) + "%")
+	var files []FetchFile
+	var fileIDs []int
+	conds = append(conds, "LIKE "+"%"+fmt.Sprint(m.ID)+"%")
+	conds = append(conds, "finished = 1")
 	if begin != nil {
 		conds = append(conds, "file_date > ?")
 		vars = append(vars, *begin)
@@ -168,9 +112,75 @@ func NeedFetch(m *orm.Material, begin, end *time.Time) bool {
 		conds = append(conds, "file_date < ?")
 		vars = append(vars, *end)
 	}
+	var fetchedFileList []orm.FileList
+	fmt.Println(conds)
+	if err := orm.DB.Model(&orm.FileList{}).Where(strings.Join(conds, " AND "), vars...).Find(&fetchedFileList).Error; err != nil {
+		return fileIDs, err
+	}
 
-	// orm.
-	// TODO: finish it
-	// TODO: 抓取FTP文件列表，对比数据库当前文件列表，然后抓取
-	return false
+	ftpFileList, err := ftpclient.GetList("./" + m.Name)
+	if err != nil {
+		return fileIDs, err
+	}
+
+	reg := regexp.MustCompilePOSIX(fileNamePattern)
+	for _, p := range ftpFileList {
+		matched := reg.FindAllStringSubmatch(p, -1)
+		if len(matched) == 0 || len(matched[0]) <= 4 {
+			continue
+		}
+		dateStr := matched[0][3]
+		fileDate, _ := time.Parse(time.RFC3339, fmt.Sprintf("%s-%s-%sT00:00:00+08:00", dateStr[:4], dateStr[4:6], dateStr[6:]))
+
+		if !fileIsNeed(&fileDate, begin, end) {
+			continue
+		}
+
+		// 根据文件名创建设备
+		createDeviceIfNotExist(matched[0][2], *m)
+
+		fetched := false
+		for _, f := range fetchedFileList {
+			if strings.Contains(f.Path, p) {
+				fetched = true
+				break
+			}
+		}
+
+		if !fetched {
+			files = append(files, FetchFile{
+				File: p,
+				Date: fileDate,
+			})
+		}
+	}
+
+	if len(files) == 0 {
+		return fileIDs, nil
+	}
+	fileIDs, err = fetchMaterialDatas(*m, files)
+	if err != nil {
+		return fileIDs, err
+	}
+
+	return fileIDs, nil
+}
+
+func fileIsNeed(fileDate, begin, end *time.Time) bool {
+	if begin == nil || end == nil {
+		return true
+	}
+	return begin.Before(*fileDate) && end.After(*fileDate)
+}
+
+func createDeviceIfNotExist(id string, material orm.Material) {
+	deviceName := fmt.Sprintf("%s设备%s", material.Name, id)
+	device := orm.GetDeviceWithName(deviceName)
+	if device == nil {
+		device = &orm.Device{
+			Name:       deviceName,
+			MaterialID: material.ID,
+		}
+		orm.DB.Create(device)
+	}
 }
