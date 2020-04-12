e156e7f23f031828cd417a02b5b337a2bd94c19f
汪波 <809754210@qq.com>
2020-04-12 23:51:43 +0800

    fix

--- ftpclient/connect.go
+++ ftpclient/connect.go
@@ -10,61 +10,53 @@ import (
 	"github.com/jlaffaye/ftp"
 )
 
-var ftpConn *ftp.ServerConn
-
-func init() {
-	connect()
-}
-
 // connect make a connection for ftp server
-func connect() error {
-	var err error
+func connect() (*ftp.ServerConn, error) {
 	ftphostConf := orm.GetSystemConfig("ftp_host")
 	if ftphostConf == nil {
-		return &FTPError{Message: "没有找到FTP服务器Host配置"}
+		return nil, &FTPError{Message: "没有找到FTP服务器Host配置"}
 	}
 
 	ftpportConf := orm.GetSystemConfig("ftp_port")
 	if ftpportConf == nil {
-		return &FTPError{Message: "没有找到FTP服务器Port配置"}
+		return nil, &FTPError{Message: "没有找到FTP服务器Port配置"}
 	}
 
 	ftpuserConf := orm.GetSystemConfig("ftp_username")
 	if ftpuserConf == nil {
-		return &FTPError{Message: "没有找到FTP服务器登录账号"}
+		return nil, &FTPError{Message: "没有找到FTP服务器登录账号"}
 	}
 
 	ftppassConf := orm.GetSystemConfig("ftp_password")
 	if ftppassConf == nil {
-		return &FTPError{Message: "没有找到FTP服务器登录密码"}
+		return nil, &FTPError{Message: "没有找到FTP服务器登录密码"}
 	}
 
-	ftpConn, err = ftp.Dial(fmt.Sprintf("%v:%v", ftphostConf.Value, ftpportConf.Value), ftp.DialWithTimeout(4*time.Second))
+	ftpConn, err := ftp.Dial(fmt.Sprintf("%v:%v", ftphostConf.Value, ftpportConf.Value), ftp.DialWithTimeout(4*time.Second))
 	if err != nil {
-		return &FTPError{
+		return nil, &FTPError{
 			Message:   fmt.Sprintf("连接FTP服务器%s:%s失败", ftphostConf.Value, ftpportConf.Value),
 			OriginErr: err,
 		}
 	}
 	err = ftpConn.Login(ftpuserConf.Value, ftppassConf.Value)
 	if err != nil {
-		return &FTPError{
+		return nil, &FTPError{
 			Message:   fmt.Sprintf("登录FTP服务器%s:%s失败", ftphostConf.Value, ftpportConf.Value),
 			OriginErr: err,
 		}
 	}
 	ftpConn.NoOp()
-	return nil
+	return ftpConn, nil
 }
 
 // ReadFile read file by path
 func ReadFile(path string) ([]byte, error) {
-	if ftpConn == nil {
-		err := connect()
-		if err != nil {
-			return nil, err
-		}
+	ftpConn, err := connect()
+	if err != nil {
+		return nil, err
 	}
+	defer ftpConn.Quit()
 	res, err := ftpConn.Retr(path)
 	defer func() {
 		if res != nil {
@@ -93,15 +85,13 @@ func ReadFile(path string) ([]byte, error) {
 
 // GetList return current workspace file list
 func GetList(path string) ([]string, error) {
-	var entries []string
-	if ftpConn == nil {
-		err := connect()
-		if err != nil {
-			return entries, err
-		}
+	ftpConn, err := connect()
+	if err != nil {
+		return nil, err
 	}
-
-	entries, err := ftpConn.NameList(path)
+	defer ftpConn.Quit()
+	var entries []string
+	entries, err = ftpConn.NameList(path)
 	if err != nil {
 		return entries, &FTPError{
 			Message:   fmt.Sprintf("获取路径%s下文件列表失败", path),
