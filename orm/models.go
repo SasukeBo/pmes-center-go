63611fcbc668f8cde95a3101cc94022fd26aa6cf
SasukeBo <809754210@qq.com>
2020-05-20 10:44:04 +0800

    优化查询

--- orm/models.go
+++ orm/models.go
@@ -50,17 +50,17 @@ type Device struct {
 
 // Product 产品表
 type Product struct {
-	ID          int    `gorm:"column:id;primary_key"`
-	UUID        string `gorm:"column:uuid;unique_index;not null"`
-	MaterialID  int    `gorm:"column:material_id;not null;index"`
-	DeviceID    int    `gorm:"column:device_id;not null"`
-	Qualified   bool   `gorm:"column:qualified;default:false"`
-	CreatedAt   time.Time
-	D2Code      string `gorm:"column:d2_code"`
-	LineID      int    `gorm:"column:line_id"`
-	JigID       string `gorm:"column:jig_id;index"`
-	MouldID     int    `gorm:"column:mould_id;index"`
-	ShiftNumber int
+	ID          int       `gorm:"column:id;primary_key"`
+	UUID        string    `gorm:"column:uuid;unique_index;not null"`
+	MaterialID  int       `gorm:"column:material_id;not null;index"`
+	DeviceID    int       `gorm:"column:device_id;not null;index"`
+	Qualified   bool      `gorm:"column:qualified;default:false"`
+	CreatedAt   time.Time `gorm:"index"`
+	D2Code      string    `gorm:"column:d2_code"`
+	LineID      int       `gorm:"column:line_id;index"`
+	JigID       string    `gorm:"column:jig_id;index"`
+	MouldID     int       `gorm:"column:mould_id;index"`
+	ShiftNumber int       `gorm:"index"`
 }
 
 // Size 尺寸
